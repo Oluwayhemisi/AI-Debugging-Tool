ab2a3da73ae408d4aaba2b3980d5f40d67b71962
Halima.Ismail <Halima.Ismail@interswitchgroup.com>
2025-07-22 12:42:22 +0100

    Dockerise my application

--- src/main/java/com/project/openAI_integration/controller/DebugController.java
+++ src/main/java/com/project/openAI_integration/controller/DebugController.java
@@ -4,6 +4,8 @@ import com.project.openAI_integration.dto.DebugRequest;
 import com.project.openAI_integration.model.Debug;
 import com.project.openAI_integration.service.DebugService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -14,8 +16,16 @@ public class DebugController {
     @Autowired
     private DebugService gptService;
 
+    @Value("${openai.api.key}")
+    private String apiKey;
+
     @PostMapping("/analyze")
     public Debug analyzeCode(@RequestBody DebugRequest debugRequest) {
         return gptService.processPrompt(debugRequest.getCode(), debugRequest.getCustomInstruction());
     }
+    @GetMapping("/test-key")
+    public ResponseEntity<String> testKey() {
+        return ResponseEntity.ok("Key is: " + (apiKey != null ? apiKey.substring(0, 5) + "..." : "MISSING"));
+    }
+
 }
